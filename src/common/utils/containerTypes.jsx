dbfc3c3b101eefbc862fa894ef3f72a1088519d1
chronosgit <warfacemeister@mail.ru>
2024-05-15 14:25:51 +0500

    feat: added common containerTypes util

---
+++ src/common/utils/containerTypes.jsx
@@ -0,0 +1,18 @@
+const componentTypes = {
+	div: <div></div>,
+	header: <header></header>,
+	footer: <footer></footer>,
+	article: <article></article>,
+	aside: <aside></aside>,
+	main: <main></main>,
+	nav: <nav></nav>,
+	section: <section></section>,
+	summary: <summary></summary>,
+	details: <details></details>,
+	figcaption: <figcaption></figcaption>,
+	figure: <figure></figure>,
+	mark: <mark></mark>,
+	time: <time></time>,
+};
+
+export default componentTypes;
