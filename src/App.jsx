6320d37f63291aefbbcab61208dedd79f52d6890
chronosgit <warfacemeister@mail.ru>
2024-05-15 14:35:42 +0500

    feat: put routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import routes from "@routes/routes";
+
 const App = () => {
 
+  const router = createBrowserRouter(routes);
+
   return (
-    <div className="">App</div>
+    <RouterProvider router={router} />
   );
 };
 
