20ef2790af3f4dffd51a6ad9eb2ce819fe3bbca8
chronosgit <warfacemeister@mail.ru>
2024-05-16 15:45:08 +0500

    feat: added posts markup with styles

--- src/modules/home/Home.jsx
+++ src/modules/home/Home.jsx
@@ -1,6 +1,7 @@
 import Container from "@common/components/container/Container";
-import HomeHeader from "./components/header/Header";
+import Header from "./components/header/Header";
 import { PostsContextProvider } from "./contexts/PostsContext";
+import Posts from "./components/posts/Posts";
 
 const Home = () => {
 
@@ -11,9 +12,9 @@ const Home = () => {
 	return (
 		<Container maxWidth={800} style={homeContainerStyles}>
 			<PostsContextProvider>
-				<HomeHeader />
+				<Header />
 
-				
+				<Posts />
 			</PostsContextProvider>
 		</Container>
 	);
